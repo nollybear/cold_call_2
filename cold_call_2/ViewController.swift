f4d37735c08d81f28496452cf3c580a7c5d4bc95
Nolan Lindeke <nlindeke@gmail.com>
2017-01-11 13:03:34 -0800

    Initial Commit

---
+++ cold_call_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cold_call_2
+//
+//  Created by Nolan Lindeke on 1/11/17.
+//  Copyright © 2017 Nolan Lindeke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
