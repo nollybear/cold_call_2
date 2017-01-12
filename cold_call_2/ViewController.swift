ef93525d6718e7071692112466c9edbf22df1ecf
Nolan Lindeke <nlindeke@gmail.com>
2017-01-12 12:31:36 -0800

    Initial commit

--- cold_call_2/ViewController.swift
+++ cold_call_2/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var nameBank = ["Bob", "Tony the Tiger", "Alexander Hamilton", "George Washington", "Benjamin Franklin", "Amy Winehouse", "Mick Jagger"]
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        numberLabel.isHidden = false
+        let length = UInt32(nameBank.count)
+        let randomNum = Int(arc4random_uniform(length))
+        nameLabel.text = String(nameBank[randomNum])
+        numberLabel.text = String(randomNum)
+        if randomNum < 3 {
+            numberLabel.textColor = UIColor.red
+        }
+        if randomNum == 3 || randomNum == 4 {
+            numberLabel.textColor = UIColor.orange
+        }
+        if randomNum >= 5 {
+            numberLabel.textColor = UIColor.green
+        }
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameLabel.text = "Ready?"
+        numberLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
